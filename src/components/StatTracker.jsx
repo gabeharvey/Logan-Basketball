c324796d6936ef2c4fc5b9371c825c7bcf73a18f
Gabe Harvey <harvey7@gmail.com>
2024-10-13 23:36:13 -0500

    Minor update to stat tracker averages box

--- src/components/StatTracker.jsx
+++ src/components/StatTracker.jsx
@@ -107,7 +107,7 @@ function StatTracker() {
             fontSize="sm"
             textAlign="center" 
           >
-            Steele HS Stats
+            Steele High School Stats
           </Text>
           <Text color="#F8F8F8">Points: {averagesHS.points}</Text>
           <Text color="#F8F8F8">Assists: {averagesHS.assists}</Text>
